2778613e6d8e1143488934666f67a8ab30c8f1b0
Siddharth Chauhan <siddharthchauhan847@gmail.com>
2024-06-20 22:31:03 +0530

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const apiKey = "bc9a5b05df5899a0926b480f4e779f8c";
+const apiKey = "";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
